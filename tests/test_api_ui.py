890170210ec97b2c65d696686241be2c6ce71829
AliWanton <hezareh22@gmail.com>
2026-02-12 14:07:28 +0100

    Update test_api_ui.py

--- tests/test_api_ui.py
+++ tests/test_api_ui.py
@@ -1,21 +1,20 @@
 import requests
 
-base_url = "https://petstore.swagger.io/v2"
+BASE_URL = "https://petstore.swagger.io/v2/swagger.json"
 def test_api_response():
-    response = requests.get(base_url)
+    response = requests.get(BASE_URL)
     assert response.status_code == 200
-    data = response.json()
-    assert "current_user_url" in data
+    assert response.headers["Content-Type"].startswith("application/json")
 
-
-def test_github_ui(page):
-    page.goto(base_url)
+def test_swagger_ui(page):
+    page.goto("https://petstore.swagger.io/")
     page.wait_for_load_state("networkidle")
+
     assert "Swagger" in page.title()
 
 
-def test_github_api():
-    response = requests.get(base_url)
+def test_petstore_api():
+    response = requests.get(BASE_URL)
 
     assert response.status_code == 200
     assert response.headers["Content-Type"].startswith("application/json")
