13f5c1192a3ed5ef147db804d11c28e1c43a5a9c
AliWanton <hezareh22@gmail.com>
2026-02-12 13:57:12 +0100

    changed api url

--- tests/test_api_ui.py
+++ tests/test_api_ui.py
@@ -1,21 +1,21 @@
 import requests
 
-
+base_url = "https://petstore.swagger.io/v2"
 def test_api_response():
-    response = requests.get("https://api.github.com")
+    response = requests.get(base_url)
     assert response.status_code == 200
     data = response.json()
     assert "current_user_url" in data
 
 
 def test_github_ui(page):
-    page.goto("https://github.com")
+    page.goto(base_url)
     page.wait_for_load_state("networkidle")
-    assert "GitHub" in page.title()
+    assert "Swagger" in page.title()
 
 
 def test_github_api():
-    response = requests.get("https://api.github.com")
+    response = requests.get(base_url)
 
     assert response.status_code == 200
     assert response.headers["Content-Type"].startswith("application/json")
